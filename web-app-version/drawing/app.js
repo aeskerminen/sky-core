fe43dafe9f1c069afdf2e291ebb55ecbcbd924da
Artturi <artturikerminen@gmail.com>
2021-10-31 15:14:33 +0200

    Change pressure.js settings

--- web-app-version/drawing/app.js
+++ web-app-version/drawing/app.js
@@ -3,10 +3,11 @@ var Pressure = require('pressure');
 Pressure.set('#canvas', {
     change: function(force, event){
         const ctx = document.querySelector('#canvas').getContext('2d')
-        let ps = Pressure.map(force, 0, 1, 0, 100);
-        ctx.lineWidth = ps
+        // let ps = Pressure.map(force, 0, 1, 0, 100);
+        ctx.lineWidth = force
+        console.log(force)
     }
-  }, {only: 'pointer'});
+  });
 
   // These are the default configs set by Pressure
 Pressure.config({
