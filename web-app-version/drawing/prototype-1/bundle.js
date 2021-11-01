f85929302279e6888acd206a05712cf73e3ceac4
Artturi <artturikerminen@gmail.com>
2021-11-01 20:12:31 +0200

    Add new prototype drawboard

--- web-app-version/drawing/prototype-1/bundle.js
+++ web-app-version/drawing/prototype-1/bundle.js
@@ -4,20 +4,20 @@ var Pressure = require('pressure');
 Pressure.set('#canvas', {
     change: function(force, event){
         const ctx = document.querySelector('#canvas').getContext('2d')
-        // let ps = Pressure.map(force, 0, 1, 0, 100);
-        ctx.lineWidth = force
+        let ps = Pressure.map(force, 0, 1, 0, 100);
+        ctx.lineWidth = ps
         console.log(force)
     }
   });
 
   // These are the default configs set by Pressure
-Pressure.config({
-    polyfill: false,
-    polyfillSpeedUp: 1000,
-    polyfillSpeedDown: 0,
-    preventSelect: true,
-    only: null
-  });
+// Pressure.config({
+//     polyfill: false,
+//     polyfillSpeedUp: 1000,
+//     polyfillSpeedDown: 0,
+//     preventSelect: true,
+//     only: null
+//   });
 },{"pressure":3}],2:[function(require,module,exports){
 let eraser = false
 
