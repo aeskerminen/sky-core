9dac749dfc5935f0cccb7d0e9fbb4f0fbf394d2c
Artturi <artturikerminen@gmail.com>
2021-11-05 21:00:25 +0200

    Add canvas to img conversion

--- web-app-version/drawing/prototype-3/js/main.js
+++ web-app-version/drawing/prototype-3/js/main.js
@@ -69,3 +69,10 @@ function changeCanvasZoom(value) {
         console.log(`Invalid zoom mode: ${value}.`)
     }
 }
+
+function download(){
+    var download = document.getElementById('download');
+    var image = document.getElementById('board').toDataURL('image/png')
+                .replace('image/png', 'image/octet-stream');
+    download.setAttribute('href', image);
+}
