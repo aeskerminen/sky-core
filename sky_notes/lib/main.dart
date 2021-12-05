7143bc84eb4dfe6a9ab9f4bfda1dad1c907d22af
Artturi Kerminen <a.kerminen11@gmail.com>
2021-12-05 20:22:09 +0200

    Abstarct widgets in main.dart

--- sky_notes/lib/main.dart
+++ sky_notes/lib/main.dart
@@ -25,75 +25,93 @@ class _MainAppState extends State<MainApp> {
     return MaterialApp(
       title: 'Sky Notes',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Row(children: [
-        Expanded(
-            flex: 1,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text("Sky Notes"),
-              ),
-              body: Container(
-                color: Colors.blueGrey,
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: const Text("file structure"),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(7.5)),
-                            color: Colors.white,
-                            border:
-                                Border.all(color: Colors.black, width: 2.5)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(3),
-                          child: Row(
-                            children: const [Text("Tag 1"), Text("Tag 2")],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )),
-        const Divider(
+      home: Row(children: const [
+        Expanded(flex: 1, child: SideBar()),
+        Divider(
           indent: 1,
           endIndent: 2,
         ),
         Expanded(
           flex: 4,
-          child: DefaultTabController(
-            length: 3,
-            child: Scaffold(
-                body: TabBarView(children: [
-                  const TextEditor(),
-                  const LatexEditor(),
-                  DrawingPage()
-                ]),
-                floatingActionButton: Align(
-                  alignment: const Alignment(0.0, 1),
-                  child: SizedBox(
-                    width: 200,
-                    height: 40,
-                    child: Container(
-                      color: Colors.black,
-                      child: const TabBar(tabs: [
-                        Tab(icon: Icon(Icons.pending_actions)),
-                        Tab(icon: Icon(Icons.pending_actions)),
-                        Tab(icon: Icon(Icons.pending_actions)),
-                      ]),
-                    ),
-                  ),
-                )),
-          ),
+          child: WorkView(),
         ),
       ]),
+    );
+  }
+}
+
+class SideBar extends StatelessWidget {
+  const SideBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sky Notes"),
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: const Text("file structure"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(7.5)),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black, width: 2.5)),
+                child: Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: Row(
+                    children: const [Text("Tag 1"), Text("Tag 2")],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WorkView extends StatelessWidget {
+  const WorkView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          body: TabBarView(children: [
+            const TextEditor(),
+            const LatexEditor(),
+            DrawingPage()
+          ]),
+          floatingActionButton: Align(
+            alignment: const Alignment(0.0, 1),
+            child: SizedBox(
+              width: 200,
+              height: 40,
+              child: Container(
+                color: Colors.black,
+                child: const TabBar(tabs: [
+                  Tab(icon: Icon(Icons.pending_actions)),
+                  Tab(icon: Icon(Icons.pending_actions)),
+                  Tab(icon: Icon(Icons.pending_actions)),
+                ]),
+              ),
+            ),
+          )),
     );
   }
 }
