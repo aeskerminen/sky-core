eecd0c2c02c87bfb8a2648911a0dde6c40a5899c
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-08 12:47:28 +0300

    Add Neutralinojs project source

---
+++ source/resources/js/main.js
@@ -0,0 +1,48 @@
+// This is just a sample app. You can structure your Neutralinojs app code as you wish.
+// This example app is written with vanilla JavaScript and HTML.
+// Feel free to use any frontend framework you like :)
+// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
+
+
+function setTray() {
+    if(NL_MODE != "window") {
+        console.log("INFO: Tray menu is only available in the window mode.");
+        return;
+    }
+    let tray = {
+        icon: "/resources/icons/trayIcon.png",
+        menuItems: [
+            {id: "VERSION", text: "Get version"},
+            {id: "SEP", text: "-"},
+            {id: "QUIT", text: "Quit"}
+        ]
+    };
+    Neutralino.os.setTray(tray);
+}
+
+function onTrayMenuItemClicked(event) {
+    switch(event.detail.id) {
+        case "VERSION":
+            Neutralino.os.showMessageBox("Version information",
+                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
+            break;
+        case "QUIT":
+            Neutralino.app.exit();
+            break;
+    }
+}
+
+function onWindowClose() {
+    Neutralino.app.exit();
+}
+
+
+Neutralino.init();
+
+
+Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
+Neutralino.events.on("windowClose", onWindowClose);
+
+if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
+    setTray();
+}
---
+++ source/resources/js/shortcut_handler.js
@@ -0,0 +1,70 @@
+document.onkeyup = function (e) {
+    var evt = window.event || e;   
+        if (evt.keyCode == 80 && evt.altKey) {
+            console.log("PRESS");   
+
+            var div = document.createElement("div");
+            div.classList.add("draggable");
+            div.classList.add("draggable-box");
+
+            var p = document.createElement("p");
+            
+            var s = document.createElement("span");
+            s.id = "answer";
+
+            p.append(s);
+            div.append(p);
+            
+            document.body.append(div);
+
+            div.onmousedown = function(e) {
+                var isMouseDown, initX, initY, height = div.offsetHeight, width = div.offsetWidth;
+                document.querySelectorAll('.draggable').forEach(div => div.style.zIndex = 0);
+                div.style.zIndex = 1;
+        
+                initX = e.offsetX;
+                initY = e.offsetY;
+                isMouseDown = true;
+        
+                window.addEventListener("mousemove", mousemove);
+                window.addEventListener("mouseup", mouseup);
+        
+                function mousemove(e){
+                    if (isMouseDown) {
+                      var cx = e.clientX - initX,
+                            cy = e.clientY - initY;
+                      if (cx < 0) {
+                        cx = 0;
+                      }
+                      if (cy < 0) {
+                        cy = 0;
+                      }
+                      if (window.innerWidth - e.clientX + initX < width) {
+                        cx = window.innerWidth - width;
+                      }
+                      if (e.clientY > window.innerHeight - height+ initY) {
+                        cy = window.innerHeight - height;
+                      }
+                      div.style.left = cx + 'px';
+                      div.style.top = cy + 'px';
+                    }
+                }
+        
+                function mouseup(){
+                    isMouseDown = false;
+                    window.removeEventListener("mousemove", mousemove);
+                    window.removeEventListener("mouseup", mouseup);
+                }
+            }
+
+            var answerSpan = document.getElementById('answer');
+            var answerMathField = MQ.MathField(answerSpan, {
+              handlers: {
+                edit: function() {
+                  var enteredMath = answerMathField.latex(); // Get entered math in LaTeX format
+                //   checkAnswer(enteredMath);
+                }
+              }
+            });
+        }
+}
