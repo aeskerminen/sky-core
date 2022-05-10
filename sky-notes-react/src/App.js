67e1b9e9693441fc28ef430fc0979309146ebbdc
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-10 21:09:30 +0300

    Rm mathquill, add react-mathquill, add LaTeX test

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { addStyles, EditableMathField } from 'react-mathquill'
 
-function App() {
+// inserts the required css to the <head> block.
+// you can skip this, if you want to do that by yourself.
+addStyles()
+
+const EditableMathExample = () => {
+  const [latex, setLatex] = useState('\\frac{1}{\\sqrt{2}}\\cdot 2')
+
   return (
-    <div></div>
-  );
+    <div>
+      <EditableMathField
+        latex={latex}
+        onChange={(mathField) => {
+          setLatex(mathField.latex())
+        }}
+      />
+      <p>{latex}</p>
+    </div>
+  )
 }
 
-export default App;
+export default EditableMathExample;
--- sky-notes-react/src/index.js
+++ sky-notes-react/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import EditableMathExample from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = <EditableMathExample></EditableMathExample>
+root.render(element);
 
 window.Neutralino.init(); // Add this function call
 
