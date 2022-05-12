574dc844bf5f3efcb80d735bc92b63f1d4e11a70
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-12 16:38:50 +0300

    Add RT editor

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
 
 import Menubutton from './menu_button';
 import LatexField from './latex_field';
 import DrawField from './draw_field';
+import RTField from './rt_field';
 
 
 class App extends React.Component {
@@ -14,6 +14,7 @@ class App extends React.Component {
         <LatexField></LatexField>
         <LatexField></LatexField>
         <DrawField></DrawField>
+        <RTField></RTField>
       </div>
     );
   }
