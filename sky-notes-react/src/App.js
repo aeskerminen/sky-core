9a12c7e8b352aeb2ac4965a43417c82f8d37a04f
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-12 15:37:25 +0300

    Add menu button

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
-import { addStyles, EditableMathField } from 'react-mathquill'
-
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import Menubutton from './menu_button';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <Menubutton></Menubutton>
       </div>
     );
   }
