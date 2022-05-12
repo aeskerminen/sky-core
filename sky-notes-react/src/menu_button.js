7476ba49d396bb72a4608a54519980580d70cd59
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-12 16:39:15 +0300

    Remove unused imports

--- sky-notes-react/src/menu_button.js
+++ sky-notes-react/src/menu_button.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
