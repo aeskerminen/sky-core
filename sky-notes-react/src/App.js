88fbe83faa1035d632662a107c8b59ad7382b312
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-12 15:47:19 +0300

    Add Latex field

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+
 import Menubutton from './menu_button';
+import LatexField from './latex_field';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Menubutton></Menubutton>
+        <LatexField></LatexField>
       </div>
     );
   }
---
+++ sky-notes-react/src/latex_field.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import Draggable from 'react-draggable';
+import { addStyles, EditableMathField } from 'react-mathquill'
+
+
+addStyles()
+
+function LatexField () {
+    const [latex, setLatex] = useState('\\frac{1}{\\sqrt{2}}\\cdot 2');
+
+    return (
+      <Draggable>
+        <div>
+          <EditableMathField
+            latex={latex}
+            onChange={(mathField) => {
+              setLatex(mathField.latex())
+            }}
+          />
+        </div>
+      </Draggable>
+        
+    );
+}
+
+export default LatexField;
+    
