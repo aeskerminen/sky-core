f2a53a54fd25579c6b14a2aabbecceba8948190e
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-12 18:10:38 +0300

    Fix RT_Field.js

--- sky-notes-react/src/RT_Field.js
+++ sky-notes-react/src/RT_Field.js
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, {Component, PropTypes} from 'react';
+import Draggable from 'react-draggable';
+import RichTextEditor from 'react-rte';
 
-function MyComponent() {
-  const [value, setValue] = useState('');
+class RTField extends Component {
+  state = {
+    value: RichTextEditor.createEmptyValue()
+  }
 
-  return (
-    <ReactQuill theme="snow" value={value} onChange={setValue}/>
-  );
+  onChange = (value) => {
+    this.setState({value});
+    if (this.props.onChange) {
+      // Send the changes up to the parent component as an HTML string.
+      // This is here to demonstrate using `.toString()` but in a real app it
+      // would be better to avoid generating a string on each change.
+      this.props.onChange(
+        value.toString('html')
+      );
+    }
+  };
+
+  render () {
+    return (
+      <Draggable>
+        <div className='box'>
+          <RichTextEditor
+          value={this.state.value}
+          onChange={this.onChange}
+          />
+        </div>
+      </Draggable>
+    );
+  }
 }
-
-export default MyComponent;
