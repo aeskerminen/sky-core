7fce0168d6bf13d12c39e7f40b1920501442684a
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-14 23:19:20 +0300

    Update app.js

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -4,17 +4,25 @@ import Menubutton from './menu_button';
 import LatexField from './latex_field';
 import DrawField from './draw_field';
 import RTField from './rt_field';
+import Appbar from './appbar';
+import NoteManager from './note_manager';
 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Menubutton></Menubutton>
-        <LatexField></LatexField>
-        <LatexField></LatexField>
-        <DrawField></DrawField>
-        <RTField></RTField>
+        <Appbar></Appbar>
+        <div>
+          <Menubutton></Menubutton>
+          <LatexField></LatexField>
+          <LatexField></LatexField>
+          <DrawField></DrawField>
+          <RTField></RTField>
+        </div>
+        <div>
+          <NoteManager></NoteManager>
+        </div>
       </div>
     );
   }
