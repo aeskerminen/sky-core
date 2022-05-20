d1b7c5cc05ad828c95a52d2f410c0597a532d263
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-20 15:41:57 +0300

    Migrate to Slate, add basic editor

--- sky-notes-react/src/rt_field.js
+++ sky-notes-react/src/rt_field.js
@@ -1,59 +1,142 @@
-import React, {Component, PropTypes} from 'react';
-import Draggable from 'react-draggable';
-import RichTextEditor from 'react-rte';
-import m_style from './handle_style';
+// Import React dependencies.
+import React, { useState, useCallback, useMemo } from 'react'
+// Import the Slate editor factory.
+import { createEditor, Editor, Transforms, Text } from 'slate'
 
-const toolbarConfig = {
-  // Optionally specify the groups to display (displayed in the order listed).
-  display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'LINK_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'HISTORY_BUTTONS'],
-  INLINE_STYLE_BUTTONS: [
-    {label: 'Bold', style: 'BOLD', className: 'custom-css-class'},
-    {label: 'Italic', style: 'ITALIC'},
-    {label: 'Underline', style: 'UNDERLINE'}
-  ],
-  BLOCK_TYPE_DROPDOWN: [
-    {label: 'Normal', style: 'unstyled'},
-    {label: 'Heading Large', style: 'header-one'},
-    {label: 'Heading Medium', style: 'header-two'},
-    {label: 'Heading Small', style: 'header-three'}
-  ],
-  BLOCK_TYPE_BUTTONS: [
-    {label: 'UL', style: 'unordered-list-item'},
-    {label: 'OL', style: 'ordered-list-item'}
-  ]
-};
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 
-class RTField extends Component {
-  state = {
-    value: RichTextEditor.createEmptyValue()
-  }
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
 
-  onChange = (value) => {
-    this.setState({value});
-    if (this.props.onChange) {
-      // Send the changes up to the parent component as an HTML string.
-      // This is here to demonstrate using `.toString()` but in a real app it
-      // would be better to avoid generating a string on each change.
-      this.props.onChange(
-        value.toString('html')
-      );
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true,
+    })
+
+    return !!match
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  },
+}
+
+
+// Define a React component renderer for our code blocks.
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+// Define a React component to render leaves with bold text.
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+} 
+
+const RTField = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
     }
-  };
+  }, [])
 
-  render () {
-    return (
-      <Draggable handle='.handle'>
-        <div className='box'>
-          <div className='handle' style={m_style}></div>
-          <RichTextEditor
-          toolbarConfig={toolbarConfig}
-          value={this.state.value}
-          onChange={this.onChange}
-          />
-        </div>
-      </Draggable>
-    );
-  }
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props}></Leaf>
+  }, [])
+
+  return(
+    <Slate editor={editor} value={initialValue}>
+      <div>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleBoldMark(editor)
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleCodeBlock(editor)
+          }}
+        >
+          Code Block
+        </button>
+      </div>
+      <Editable
+        editor={editor}
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if(!event.ctrlKey)
+            return
+
+          switch(event.key) {
+            case "'": {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock(editor);
+              break;
+            }
+            case 'b': {
+              event.preventDefault();
+              CustomEditor.toggleBoldMark(editor);
+              break;
+            }
+          }
+        }}
+      ></Editable>
+    </Slate>
+  );
+
 }
 
 export default RTField;
