d9e6e2968fbd1df4d69eed2804367e9b92b1ae4c
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-23 16:40:55 +0300

    New UI layout

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -1,35 +1,28 @@
 import * as React from 'react';
 
-import Menubutton from './menu_button';
-import LatexField from './latex_field';
-import DrawField from './draw_field';
-import RTField from './rt_field';
-import Appbar from './appbar';
-import NoteManager from './note_manager';
-import { Paper } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Appbar></Appbar>
-        {/* <Menubutton></Menubutton> */}
-
         <Grid container spacing={0}>
           <Grid item xs={2}>
-            <Paper elevation={5} style={{overflowY: 'auto', backgroundColor: 'cream'}} sx={{m:2, height: '91.5vh'}}>
-            </Paper>
+            <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
+            </Box>
           </Grid>
           <Grid item xs={10}>
-            <Paper elevation={5} style={{overflowY: 'auto', backgroundColor: 'cream'}} sx={{m:2, height: '91.5vh'}}>
-              <RTField></RTField>
-            </Paper>
+              <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
+              <Stack direction="row" spacing={0}>
+                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+              </Stack>
+              <Box sx={{backgroundColor: 'white', m:0, p:0, height: '96%' }}></Box>
+              </Box>
           </Grid>
         </Grid>
-        
-      </div>
     );
   }
 }
