f2e1c26f93f646ea7c66ffdf2a0ee20f96d8adb0
Artturi Kerminen <kerminenartturi@gmail.com>
2022-05-27 15:30:29 +0300

    Remove margin and padding

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -11,17 +11,17 @@ class App extends React.Component {
     return (
         <Grid container spacing={0}>
           <Grid item xs={2}>
-            <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
+            <Box sx={{backgroundColor: 'DarkGray', m:0, p:0, height: '100vh' }}>
             </Box>
           </Grid>
           <Grid item xs={10}>
-              <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
+              <Box sx={{backgroundColor: 'Gray', m:0, p:0, height: '100vh' }}>
               <Stack direction="row" spacing={0}>
-                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
-                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
-                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+                <Box sx={{backgroundColor: 'DimGray', p:1, minWidth: '5%'}}>Box 1</Box>
+                <Box sx={{backgroundColor: 'Gray', p:1, minWidth: '5%'}}>Box 1</Box>
+                <Box sx={{backgroundColor: 'Gray', p:1, minWidth: '5%'}}>Box 1</Box>
               </Stack>
-                <Box sx={{backgroundColor: 'white', m:0, p:1, height: '94.25%' }}>
+                <Box sx={{backgroundColor: 'DimGray', m:0, p:1, height: '94.25%' }}>
                   {/* <RTField xs={{m:2}}></RTField> */}
                   <DrawField xs={{m:2}}></DrawField>
                 </Box>
