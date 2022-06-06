4d5a3b5a3a515d4981281b4d96ef6904f906ccf8
Artturi Kerminen <kerminenartturi@gmail.com>
2022-06-06 10:21:42 +0300

    Change color scheme

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -11,21 +11,14 @@ class App extends React.Component {
     return (
         <Grid container spacing={0}>
           <Grid item xs={2}>
-            <Box sx={{backgroundColor: 'DarkGray', m:0, p:0, height: '100vh' }}>
+            <Box sx={{backgroundColor: '#b3e5fc', m:0, p:0, height: '100vh' }}>
+
             </Box>
           </Grid>
           <Grid item xs={10}>
-              <Box sx={{backgroundColor: 'Gray', m:0, p:0, height: '100vh' }}>
-              <Stack direction="row" spacing={0}>
-                <Box sx={{backgroundColor: 'DimGray', p:1, minWidth: '5%'}}>Box 1</Box>
-                <Box sx={{backgroundColor: 'Gray', p:1, minWidth: '5%'}}>Box 1</Box>
-                <Box sx={{backgroundColor: 'Gray', p:1, minWidth: '5%'}}>Box 1</Box>
-              </Stack>
-                <Box sx={{backgroundColor: 'DimGray', m:0, p:1, height: '94.25%' }}>
-                  {/* <RTField xs={{m:2}}></RTField> */}
-                  <DrawField xs={{m:2}}></DrawField>
+              <Box sx={{backgroundColor: '#4fc3f7', m:0, p:1, height: '97.7vh' }}>
+                  <RTField xs={{m:2}}></RTField>
                 </Box>
-              </Box>
           </Grid>
         </Grid>
     );
