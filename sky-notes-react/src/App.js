e8f6690380142283899f9e8e0e08e2bacf0c7978
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2022-06-07 11:42:57 +0300

    Working Splash Screen

--- sky-notes-react/src/App.js
+++ sky-notes-react/src/App.js
@@ -1,34 +1,48 @@
 import * as React from 'react';
+import {useEffect, useState} from "react";
 
 import { Box, Typography, Stack } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 import RTField from './rt_field';
+import SplashScreen from './SplashScreen'
 
+const App = () => {
+  
+  // Loading state 
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+  
+    // Wait for 3 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
-class App extends React.Component {
-  render() {
-    return (
-        <Grid container spacing={0}>
-          <Grid item xs={2}>
-            <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
-            </Box>
-          </Grid>
-          <Grid item xs={10}>
-              <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
-              <Stack direction="row" spacing={0}>
-                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
-                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
-                <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
-              </Stack>
-                <Box sx={{backgroundColor: 'white', m:0, p:1, height: '94.25%' }}>
-                  <RTField xs={{m:2}}></RTField>
-                </Box>
-              </Box>
-          </Grid>
+return isLoading ? 
+  <SplashScreen></SplashScreen> :
+  (
+      <Grid container spacing={0}>
+        <Grid item xs={2}>
+          <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
+          </Box>
         </Grid>
-    );
-  }
+        <Grid item xs={10}>
+            <Box sx={{backgroundColor: 'lightBlue', m:1, p:1, height: '96.5vh' }}>
+            <Stack direction="row" spacing={0}>
+              <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+              <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+              <Box sx={{backgroundColor: 'gray', p:1, minWidth: '5%'}}>Box 1</Box>
+            </Stack>
+              <Box sx={{backgroundColor: 'white', m:0, p:1, height: '94.25%' }}>
+                <RTField xs={{m:2}}></RTField>
+              </Box>
+            </Box>
+        </Grid>
+      </Grid>
+  );
 }
 
 export default App;
+
