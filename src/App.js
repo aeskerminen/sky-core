7b1590b98a756003528f22032ae1086cbeb07146
Artturi Kerminen <a.kerminen11@gmail.com>
2022-08-23 18:19:30 +0300

    Pass selected note upwards in state to App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import { ReactSketchCanvas } from 'react-sketch-canvas';
 import Editor from './Editor';
 import NoteExplorer from './NoteExplorer';
 
+
+
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      selectedNote: ''
+    }
   }
 
+  getSelectedNote(id) {
+    this.setState(() => ({
+      selectedNote: id
+    }))
+  }
+  
   render() {
     return (
       <div className='h-screen'>
@@ -19,9 +31,9 @@ class App extends React.Component {
         </div>
         <div className='p-1' style={{ height: '94%' }}>
           <div className='grid grid-cols-5 gap-2 h-full'>
-            <NoteExplorer></NoteExplorer>
+            <NoteExplorer selecetdCallback={this.getSelectedNote.bind(this)}></NoteExplorer>
             <div className='col-span-2 bg-slate-200 rounded drop-shadow-lg no-scrollbar overflow-y-auto p-2'>
-              <Editor className='h-full' value={""}></Editor>
+              <Editor className='h-full' selected={this.state.selectedNote} value={""} ></Editor>
             </div>
             <div className='col-span-2 bg-slate-200 rounded drop-shadow-lg no-scrollbar overflow-y-auto p-2'>
               <ReactSketchCanvas className='p-2 h-full'
