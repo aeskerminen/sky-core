7f43ee11f7499247060047855960ea05a32d7787
Artturi Kerminen <a.kerminen11@gmail.com>
2022-09-04 14:03:21 +0300

    Remove redundant imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React from 'react'
-import { ReactSketchCanvas } from 'react-sketch-canvas';
-
-import Editor from './Editor';
-import NoteExplorer from './NoteExplorer';
-
-import { AiFillBook } from 'react-icons/ai'
 import Sidebar from './Sidebar';
 
 class App extends React.Component {
