f4954ca613944e7c7ca73bf7dc267191d65795c6
Artturi Kerminen <a.kerminen11@gmail.com>
2022-09-05 20:51:21 +0300

    Make grid layout for books / notes

--- src/Books.js
+++ src/Books.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+function note() {
+    return(
+        <div>HELLO</div>
+    )
+}
+
 export default class Books extends React.Component {
     render() {
         return(
-            <div>
-                <h1>BOOKS</h1>
+            <div className='' style={{ height: '100%', gridColumn: 'span 31 / span 31' }}>
+                <div className="h-full grid grid-rows-2 p-2 gap-2">
+                    <div className="p-2 bg-slate-300">
+                        dsd
+                    </div>
+                    <div className="p-2 bg-slate-300">
+                        sdsad
+                    </div>
+                </div>
             </div>
         )
     }
