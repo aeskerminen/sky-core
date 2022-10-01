e06d926e754c90c9c5970943627a31dd554d3226
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-02 00:54:24 +0300

    Add Sidebar component

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Sidebar></Sidebar>
     </div>
   );
 }
