37e5c6f66f324969c6c9464a8082d2ac7379d627
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-02 01:43:23 +0300

    Add basic controls to Sidebar

--- sky-core/src/components/Sidebar.js
+++ sky-core/src/components/Sidebar.js
@@ -3,13 +3,23 @@ import React from "react";
 export default class Sidebar extends React.Component {
     render() {
         return(
-            <div className="bg-blue-500 h-full">
-                <div className="flex items-center p-2 bg-blue-700">
-                    <input class="placeholder:text-slate-400 block w-full mr-2 bg-white border border-slate-300 rounded-lg py-2 pl-3 pr-3 
-                    shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
-                    placeholder="Search for anything..." type="text" name="search"/>
+            <div className="flex flex-col bg-slate-300 h-full p-2 gap-1 shadow-md overflow-hidden">
+                <div className=" flex items-center p-1.5 bg-white">
+                    <input class="placeholder:text-slate-400 block w-full mr-2 bg-white border 
+                    border-black rounded-lg py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:ring-1 
+                    sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
                     
-                    <button className="p-2 bg-white rounded-lg ml-auto">ADD</button>
+                    <button className="p-2 bg-white rounded-lg ml-auto hover:bg-slate-100">ADD</button>
+                </div>
+                <div className="grow flex flex-col overflow-y-scroll scroll p-2 gap-2 bg-white">
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
+                    <span className="block py-20 bg-slate-100">NOTE</span>
                 </div>
             </div>
         )
