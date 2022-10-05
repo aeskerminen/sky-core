cf2023505a96241dcbe0a07f6b1b2283091bf5a3
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-06 00:00:56 +0300

    Add Jodit editor

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+
+import JoditEditor from 'jodit-react';
+
+function Jodit() {
+  const editor = useRef(null);
+  const [content, setContent] = useState("Start writing");
+  const config = {
+    readonly: false,
+    height: 400
+  };
+  const handleUpdate = (event) => {
+    const editorContent = event.target.innerHTML;
+    setContent(editorContent);
+  };
+
+  return (
+    <div className="App">
+      <JoditEditor
+        ref={editor}
+        value={content}
+        config={config}
+        onBlur={handleUpdate}
+        onChange={(newContent) => {}}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -14,10 +41,10 @@ function App() {
           <Sidebar></Sidebar>
         </div>
         <div className="grow w-5/6 overflow-hidden">
-          
+          {Jodit()}
         </div>
       </div>
-      
+
     </div>
   );
 }
