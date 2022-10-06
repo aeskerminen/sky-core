ac63437c873c09a3e0219afec6908ab361a06a1a
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-06 23:45:27 +0300

    Change bg color

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -34,7 +34,7 @@ function Jodit() {
 
 function App() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col bg-slate-500">
       <div className="flex-none">
         <Navbar></Navbar>
       </div>
@@ -42,7 +42,7 @@ function App() {
         <div className="grow w-1/6 overflow-hidden">
           <Sidebar></Sidebar>
         </div>
-        <div className="grow w-5/6 overflow-hidden m-2 mr-1">
+        <div className="grow w-5/6 overflow-hidden m-2 mr-1 bg-white">
           {Jodit()}
           <Drawboard width={window.innerWidth * 0.825} height={500}></Drawboard>
         </div>
