ae94fc54134dd3c548e02fc07fdd092297e0aba5
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-06 23:39:12 +0300

    Add Drawboard to layout

---
+++ sky-core/src/components/widgets/Drawboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import CanvasDraw from 'react-canvas-draw'
+
+export default class Drawboard extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            c_width: props.width,
+            c_height: props.height
+        }
+    }
+
+    render() {
+        return(
+            <div className="p-2 bg-slate-100 w-full">
+                <CanvasDraw canvasWidth={this.state.c_width} canvasHeight={this.state.c_height}></CanvasDraw>
+            </div>
+        )
+    }
+}
