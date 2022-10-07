44a0387d93951abb0baed3f028bb33777f0018f7
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-07 23:25:27 +0300

    Change spacing

--- sky-core/src/components/Sidebar.js
+++ sky-core/src/components/Sidebar.js
@@ -5,7 +5,7 @@ import Note from "./notes/Note";
 export default class Sidebar extends React.Component {
     render() {
         return(
-            <div className="flex flex-col bg-slate-300 h-full m-2 mr-1 shadow-md overflow-hidden">
+            <div className="flex flex-col bg-slate-300 h-full m-2 p-1 mr-1 shadow-md overflow-hidden">
                 <div className=" flex items-center p-1.5 bg-white">
                     <input class="placeholder:text-slate-400 block w-full mr-2 bg-white border 
                     border-black rounded-lg py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:ring-1 
