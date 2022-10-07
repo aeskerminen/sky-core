54f6b4a45088cd9cca1f60d4a4fb548704050b06
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-07 23:17:21 +0300

    Change spacing

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -39,10 +39,10 @@ function App() {
         <Navbar></Navbar>
       </div>
       <div className="h-screen flex flex-row">
-        <div className="grow w-1/6 overflow-hidden">
+        <div className="grow w-1/6 overflow-hidden mb-2.5">
           <Sidebar></Sidebar>
         </div>
-        <div className="grow w-5/6 overflow-hidden m-2 mr-1 bg-white">
+        <div className="grow w-5/6 overflow-hidden m-2 mr-2 mb-2.5 bg-white">
           {Jodit()}
           <Drawboard width={window.innerWidth * 0.825} height={500}></Drawboard>
         </div>
