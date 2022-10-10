b46221c4768448e52a88c1198a53c698929971a2
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-10 21:46:52 +0300

    Add name prop

--- sky-core/src/components/notes/Note.js
+++ sky-core/src/components/notes/Note.js
@@ -14,7 +14,7 @@ export default class Note extends React.Component {
         return(
             <div className="flex flex-col p-2 mr-1 ml-1 bg-slate-100 space-y-2 shadow-md">
                 <div className="flex flex-row items-center">
-                    <span className="font-normal tracking-normal">MAA13 muistiinpanot</span>
+                    <span className="font-normal tracking-normal">{this.props.name}</span>
                     <button className="bg-white hover:bg-gray-50 active:bg-gray-100 border border-black px-3 py-1 ml-auto rounded-full">x</button>
                 </div>
                 <div className="border border-t-2 border-t-black"></div>
