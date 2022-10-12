76be36c7b33633b584daee82abef82ba31c09b91
Artturi Kerminen <a.kerminen11@gmail.com>
2022-10-12 20:55:48 +0300

    Implement note deletion

--- sky-core/src/components/Sidebar.js
+++ sky-core/src/components/Sidebar.js
@@ -1,24 +1,34 @@
 import React from "react";
 import Note from "./notes/Note";
 
+let idCounter = '0'
 
 export default class Sidebar extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            notes: ['MAA13', 'MAA14'],
-            input_state: ''
+            notes: [{name: 'MAA13', id: idCounter, content: 'dsadsadas'}],
+            input_state: '',
         }
 
-        this.addNote.bind(this)
+        this.addNote = this.addNote.bind(this)
+        this.removeNote = this.removeNote.bind(this)
     }
 
-    addNote(_name) {
+    addNote(note) {
         this.setState(((state) => ({
-            notes: [...state.notes, _name]
+            notes: [...state.notes, note]
         })));
     }
+
+    removeNote(note) {
+        console.log(note)
+
+        this.setState(((state) => ({
+            notes: state.notes.filter((_note) => {return _note.id !== note.id})
+        })));
+    }  
 
     handleInputChange(event) {
         this.setState(((state) => ({
@@ -35,12 +45,12 @@ export default class Sidebar extends React.Component {
                     border-black rounded-lg py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:ring-1 
                     sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
                     
-                    <button onClick={() => { this.addNote(this.state.input_state) }} className="p-2 bg-white rounded-lg ml-auto hover:bg-slate-100">ADD</button>
+                    <button onClick={() => { idCounter+= 1; this.addNote({name: this.state.input_state, id: idCounter, content: ''}) }} className="p-2 bg-white rounded-lg ml-auto hover:bg-slate-100">ADD</button>
                 </div>
                 <div className="grow flex flex-col p-2 gap-3 bg-white overflow-y-scroll">
-                    {this.state.notes.map((name) => <Note name={name}></Note>)}
+                    {this.state.notes.map((note) => <Note removeButtonClick={this.removeNote} id={note.id} content={note.content} name={note.name}></Note>)}
                 </div>
             </div>
         )
     }
-}
+}   
--- sky-core/src/components/notes/Note.js
+++ sky-core/src/components/notes/Note.js
@@ -10,12 +10,23 @@ function tag() {
 }
 
 export default class Note extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.handleRemoveButtonClick = this.handleRemoveButtonClick.bind(this)
+    }
+
+    handleRemoveButtonClick() { 
+        this.props.removeButtonClick({name: this.props.name, id: this.props.id, content: this.props.content})
+    }
+
     render() {
         return(
             <div className="flex flex-col p-2 mr-1 ml-1 bg-slate-100 space-y-2 shadow-md">
                 <div className="flex flex-row items-center">
                     <span className="font-normal tracking-normal">{this.props.name}</span>
-                    <button className="bg-white hover:bg-gray-50 active:bg-gray-100 border border-black px-3 py-1 ml-auto rounded-full">x</button>
+                    <button onClick={this.handleRemoveButtonClick} className="bg-white hover:bg-gray-50 active:bg-gray-100 border border-black px-3 py-1 ml-auto rounded-full">x</button>
                 </div>
                 <div className="border border-t-2 border-t-black"></div>
                 <div className="">
