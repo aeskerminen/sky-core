bf83382c8ca562bf555f68f2cbe79ee517b12c27
Artturi Kerminen <a.kerminen11@gmail.com>
2022-11-07 16:52:21 +0200

    Add Editor

---
+++ sky-core/src/components/widgets/Editor.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default class Editor extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <textarea className="w-full">
+                    
+                </textarea>
+            </div>
+        )
+    }
+}
