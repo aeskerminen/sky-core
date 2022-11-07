a1d88b83bac4052d206fb1018bf2035e2c205556
Artturi Kerminen <a.kerminen11@gmail.com>
2022-11-07 16:47:37 +0200

    Remove unsused imports

--- sky-core/src/components/widgets/Drawboard.js
+++ sky-core/src/components/widgets/Drawboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import CanvasDraw from 'react-canvas-draw'
 
 export default class Drawboard extends React.Component {
 
@@ -15,7 +14,6 @@ export default class Drawboard extends React.Component {
     render() {
         return(
             <div className="p-2 bg-slate-100 w-full">
-                <CanvasDraw canvasWidth={this.state.c_width} canvasHeight={this.state.c_height}></CanvasDraw>
             </div>
         )
     }
