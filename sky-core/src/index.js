8d769169eed5dc2754904ea263417b9256b4c4bc
Artturi Kerminen <a.kerminen11@gmail.com>
2022-11-19 13:01:42 +0200

    Add context to index.js

--- sky-core/src/index.js
+++ sky-core/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { AuthProvider } from './ctx/AuthProvider';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
