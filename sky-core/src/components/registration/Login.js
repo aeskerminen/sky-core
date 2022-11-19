a642322f5690de65e7d968484a4a512a39ea0c82
Artturi Kerminen <a.kerminen11@gmail.com>
2022-11-19 13:01:26 +0200

    Add Login form and Auth provider

---
+++ sky-core/src/components/registration/Login.js
@@ -0,0 +1,66 @@
+import { useRef, useState, useEffect } from "react";
+
+import React from "react";
+
+const Login = () => {
+
+    const userRef = useRef()
+    const errRef = userRef()
+
+    const [user, setUser] = useState('')
+    const [pwd, setPwd] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setUser('')
+        setPwd('')
+        setSuccess(true)
+    }
+
+    return (
+        <>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="#">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? 'errMsg' : 'offscreen'} aria-live="assertive">{errMsg}</p>
+                    <h1>Sign in</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" ref={userRef} autoComplete="off" onChange={(e) => { setUser(e.target.value) }} value={user} required></input>
+
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" onChange={(e) => { setPwd(e.target.value) }} value={pwd} required></input>
+
+                        <button>Sign In</button>
+                    </form>
+                    <p>
+                        Need an Account?<br />
+                        <span className="line">
+                            {/*router link*/}
+                            <a href="#">Sign Up</a>
+                        </span>
+                    </p>
+                </section>
+            )}
+        </>
+    )
+}
+
+export default Login
