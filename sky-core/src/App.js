309206a8b2e4a67aa96bb1550e08150bc176b787
Artturi Kerminen <a.kerminen11@gmail.com>
2022-11-29 13:49:04 +0200

    Remove Axios auth

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -1,27 +1,27 @@
-import React, {useState, useRef} from "react";
+import React from "react";
 
 import Navbar from "./components/Navbar";
+import Register from "./components/registration/Register";
 import Sidebar from "./components/Sidebar";
-
-
-import Drawboard from "./components/widgets/Drawboard";
+import Editor from "./components/widgets/Editor";
+import Login from "./components/registration/Login";
 
 
 function App() {
   return (
     <div className="h-screen flex flex-col bg-slate-500 overflow-hidden">
-      <div className="flex-none">
+      {/* <div className="flex-none">
         <Navbar></Navbar>
       </div>
       <div className="h-screen flex flex-row">
         <div className="grow w-1/6 overflow-hidden mb-2.5">
           <Sidebar></Sidebar>
         </div>
-        <div className="grow w-5/6 overflow-hidden m-2 mr-2 mb-2.5 p-1 bg-white">
-          <Drawboard width={window.innerWidth * 0.818} height={500}></Drawboard>
-        </div>
-      </div>
 
+        <div className="grow w-5/6 overflow-hidden m-2 mr-2 mb-2.5 p-1 bg-white">
+        </div>
+      </div> */}
+      <Login></Login>
     </div>
   );
 }
