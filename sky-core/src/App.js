506f539d73aa2de28ab333841728477bd1b493e9
Artturi Kerminen <a.kerminen11@gmail.com>
2022-12-01 21:04:03 +0200

    Conditional rendering based on auth status

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -7,22 +7,37 @@ import Editor from "./components/widgets/Editor";
 import LoginButton from './components/login/LoginButton'
 import LogoutButton from './components/login/LogoutButton'
 
-function App() {
+import { useAuth0 } from "@auth0/auth0-react";
+
+
+const App = () => {
+  const { isAuthenticated } = useAuth0()
+
   return (
     <div className="h-screen flex flex-col bg-slate-500 overflow-hidden">
-      {/* <div className="flex-none">
-        <Navbar></Navbar>
-      </div>
-      <div className="h-screen flex flex-row">
-        <div className="grow w-1/6 overflow-hidden mb-2.5">
-          <Sidebar></Sidebar>
-        </div>
+      {isAuthenticated &&
+        <div>
+          <div className="flex-none">
+            <LogoutButton></LogoutButton>
+            <Navbar>
+            </Navbar>
+          </div>
+          <div className="h-screen flex flex-row">
+            <div className="grow w-1/6 overflow-hidden mb-2.5">
+              <Sidebar></Sidebar>
+            </div>
 
-        <div className="grow w-5/6 overflow-hidden m-2 mr-2 mb-2.5 p-1 bg-white">
+            <div className="grow w-5/6 overflow-hidden m-2 mr-2 mb-2.5 p-1 bg-white">
+            </div>
+          </div>
         </div>
-      </div> */}
-      <LoginButton></LoginButton>
-      <LogoutButton></LogoutButton>
+      }
+      {!isAuthenticated &&
+        <div>
+          <LoginButton></LoginButton>
+
+        </div>
+      }
     </div>
   );
 }
