0001bb2eae5a7a97e28c2341dab9f2700260be89
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2022-12-13 18:01:43 +0200

    Add single-page view

--- sky-core/src/components/widgets/PDF/PDFViewer.js
+++ sky-core/src/components/widgets/PDF/PDFViewer.js
@@ -9,7 +9,6 @@ export default function PDFViewer() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [content, setContent] = useState(0);
-  const [val, setVal] = useState(1);
 
   async function getTheFile() {
     // open file picker
@@ -20,8 +19,20 @@ export default function PDFViewer() {
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+    setPageNumber(1);
   }
- 
+
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
 
   return (
     <div>
@@ -30,7 +41,7 @@ export default function PDFViewer() {
                     <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
                     <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
                 </svg>
-                <span>Upload</span>
+                <span>Upload PDF</span>
             </button>
             <div>
               <input type="text" placeholder='Search...'/>
@@ -40,10 +51,21 @@ export default function PDFViewer() {
     file={content}
     onLoadSuccess={onDocumentLoadSuccess}
     >
-    {Array.from(new Array(numPages), (el, index) => (
-        <Page renderAnnotationLayer={false} key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
+        <Page renderAnnotationLayer={false} pageNumber={pageNumber} />
+    
     </Document>
+    <div>
+    <p>
+          Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+        </p>
+    <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+      Previous
+    </button>
+    <button type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
+      Next
+    </button>
+
+    </div>
 
     </div>
   );
