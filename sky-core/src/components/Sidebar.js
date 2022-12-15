dfcf5460a2773d1eb49538ea165b35bb614defc0
Artturi Kerminen <a.kerminen11@gmail.com>
2022-12-15 13:58:03 +0200

    Fix typo

--- sky-core/src/components/Sidebar.js
+++ sky-core/src/components/Sidebar.js
@@ -41,7 +41,7 @@ export default class Sidebar extends React.Component {
         return(
             <div className="flex flex-col bg-slate-300 h-full m-2 p-1 mr-1 shadow-md overflow-hidden">
                 <div className=" flex items-center p-1.5 bg-white">
-                    <input onChange={this.handleInputChange.bind(this)} class="placeholder:text-slate-400 block w-full mr-2 bg-white border 
+                    <input onChange={this.handleInputChange.bind(this)} className="placeholder:text-slate-400 block w-full mr-2 bg-white border 
                     border-black rounded-lg py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:ring-1 
                     sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
                     
