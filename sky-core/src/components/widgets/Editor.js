2078355293dbbe616a8d8d8861a46ec9d99ce6bd
Artturi Kerminen <a.kerminen11@gmail.com>
2022-12-15 14:05:36 +0200

    Add serialization to JSON

--- sky-core/src/components/widgets/Editor.js
+++ sky-core/src/components/widgets/Editor.js
@@ -1,140 +1,42 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useMemo } from "react";
 
 // Import the Slate editor factory.
 import { createEditor, Editor, Transforms, Text } from 'slate'
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react'
 
-const initialValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-]
-const CodeElement = props => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-
-const DefaultElement = props => {
-  return <p {...props.attributes}>{props.children}</p>
-}
-
-const Leaf = props => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
-    >
-      {props.children}
-    </span>
-  )
-}
-
-// Define our own custom set of helpers.
-const CustomEditor = {
-  isBoldMarkActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.bold === true,
-      universal: true,
-    })
-
-    return !!match
-  },
-
-  isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: n => n.type === 'code',
-    })
-
-    return !!match
-  },
-
-  toggleBoldMark(editor) {
-    const isActive = CustomEditor.isBoldMarkActive(editor)
-    Transforms.setNodes(
-      editor,
-      { bold: isActive ? null : true },
-      { match: n => Text.isText(n), split: true }
-    )
-  },
-
-  toggleCodeBlock(editor) {
-    const isActive = CustomEditor.isCodeBlockActive(editor)
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : 'code' },
-      { match: n => Editor.isBlock(editor, n) }
-    )
-  },
-}
 
 const TextEditor = () => {
-  const [editor] = useState(() => withReact(createEditor()))
+    const [editor] = useState(() => withReact(createEditor()))
 
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
+    const initialValue = useMemo(
+        () =>
+            JSON.parse(localStorage.getItem('content')) || [
+                {
+                    type: 'paragraph',
+                    children: [{ text: 'A line of text in a paragraph.' }],
+                },
+            ],
+        []
+    )
 
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />
-  }, [])
-
-  return (
-    // Add a toolbar with buttons that call the same methods.
-    <Slate editor={editor} value={initialValue}>
-      <div>
-        <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleBoldMark(editor)
-          }}
-        >
-          Bold
-        </button>
-        <button
-          onMouseDown={event => {
-            event.preventDefault()
-            CustomEditor.toggleCodeBlock(editor)
-          }}
-        >
-          Code Block
-        </button>
-      </div>
-      <Editable
-        editor={editor}
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={event => {
-          if (!event.ctrlKey) {
-            return
-          }
-
-          switch (event.key) {
-            case '`': {
-              event.preventDefault()
-              CustomEditor.toggleCodeBlock(editor)
-              break
-            }
-
-            case 'b': {
-              event.preventDefault()
-              CustomEditor.toggleBoldMark(editor)
-              break
-            }
-          }
-        }}
-      />
-    </Slate>
-  )
+    return (
+        <Slate
+            editor={editor}
+            value={initialValue}
+            onChange={value => {
+                const isAstChange = editor.operations.some(
+                    op => 'set_selection' !== op.type
+                )
+                if (isAstChange) {
+                    // Save the value to Local Storage.
+                    const content = JSON.stringify(value)
+                    localStorage.setItem('content', content)
+                }
+            }}>
+            <Editable />
+        </Slate>
+    )
 }
 
 
