19786e02fd5c1ac9fd8692f5c484cc3a4a045885
Artturi Kerminen <a.kerminen11@gmail.com>
2022-12-18 16:39:38 +0200

    Add saving and loading

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -1,25 +1,36 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useMemo, useCallback, useEffect } from "react";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import LoginButton from './components/login/LoginButton'
-import TextEditor from "./components/widgets/Editor";
+// import TextEditor from "./components/widgets/Editor";
 
-import { useAuth0 } from "@auth0/auth0-react";
-
-let selection = '0'
-
-function setSelectedNote(id) {
-  selection = id
-  console.log(selection)
-}
-
-function getSelectedNote() {
-  return selection
-}
+import { useAuth0, withAuth0 } from "@auth0/auth0-react";
+import { Editor, createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react"
 
 const App = () => {
+
   const { isAuthenticated } = useAuth0()
+  const [editor] = useState(() => withReact(createEditor()))
+  const [selection, setSelection] = useState('0')
+
+  const initialValue = useMemo(
+    () =>
+      JSON.parse(localStorage.getItem(selection)) || [
+        {
+          type: 'paragraph',
+          children: [{ text: 'A line of text in a pa  ragraph.' }],
+        },
+      ],
+    [selection]
+  )
+
+  useEffect(() => {
+    editor.children = JSON.parse(localStorage.getItem(selection))
+    editor.onChange()
+
+  }, [selection])
 
   return (
     <div className="h-screen flex flex-col bg-slate-500 overflow-hidden">
@@ -31,11 +42,25 @@ const App = () => {
           </div>
           <div className="h-screen flex flex-row">
             <div className="grow w-1/6 overflow-hidden mb-2.5">
-              <Sidebar selectButtonClick={setSelectedNote}></Sidebar>
+              <Sidebar selectButtonClick={(id) => {setSelection(id)}}></Sidebar>
             </div>
-
             <div className="grow w-5/6 overflow-y-scroll m-2 mr-2 mb-2.5 p-1 bg-white">
-              <TextEditor id={getSelectedNote}></TextEditor>
+              <Slate
+                editor={editor}
+                value={initialValue}
+                onChange={value => {
+                  const isAstChange = editor.operations.some(
+                    op => 'set_selection' !== op.type
+                  )
+                  if (isAstChange) {
+                    // Save the value to Local Storage.
+                    const content = JSON.stringify(value)
+                    localStorage.setItem(selection, content)
+                  }
+                }}
+              >
+                <Editable />
+              </Slate>
             </div>
           </div>
         </div>
@@ -46,6 +71,7 @@ const App = () => {
     </div>
   );
 }
+
 
 const LoginPage = () => {
   return (
@@ -65,4 +91,4 @@ const LoginPage = () => {
   )
 }
 
-export default App;
+export default withAuth0(App);
