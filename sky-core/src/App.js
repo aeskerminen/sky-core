feb8edc2e8c675119e5815d0603e381bc3184757
Artturi Kerminen <a.kerminen11@gmail.com>
2022-12-26 16:25:08 +0200

    Add user data saving

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -9,8 +9,37 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
+// DATABASE
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, update } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnYWP06n84p_vLSQpUPYdIapnhyh2H9Zw",
+  authDomain: "skm2022-3667a.firebaseapp.com",
+  projectId: "skm2022-3667a",
+  storageBucket: "skm2022-3667a.appspot.com",
+  messagingSenderId: "163232549889",
+  appId: "1:163232549889:web:8bf0aa897e1f9eb01e4400",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
+function writeNoteData(userId, note) {
+  const { selection, content } = note;
+  update(ref(db, "users/" + userId + "/notes/" + selection), {
+    content: content,
+  });
+}
+
+// DATABASE
+
 const App = () => {
   const { isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
   const [editor] = useState(() => withReact(createEditor()));
   const [selection, setSelection] = useState("0");
 
@@ -26,7 +55,8 @@ const App = () => {
   );
 
   useEffect(() => {
-    editor.children = JSON.parse(localStorage.getItem(selection));
+    // editor.children = JSON.parse(localStorage.getItem(selection));
+    //editor.children = JSON.parse();
     editor.onChange();
   }, [selection]);
 
@@ -35,13 +65,16 @@ const App = () => {
       editor={editor}
       value={initialValue}
       onChange={(value) => {
+        const { sub } = user;
+
         const isAstChange = editor.operations.some(
           (op) => "set_selection" !== op.type
         );
         if (isAstChange) {
           // Save the value to Local Storage.
           const content = JSON.stringify(value);
-          localStorage.setItem(selection, content);
+          writeNoteData(sub, { selection, content });
+          //localStorage.setItem(selection, content);
         }
       }}
     >
@@ -55,6 +88,14 @@ const App = () => {
         <div>
           <div className="flex-none">
             <Navbar></Navbar>
+            <button
+              onClick={() => {
+                const { sub } = user;
+                writeNoteData(sub, "Hello yes");
+              }}
+            >
+              SAVE
+            </button>
           </div>
           <div className="h-screen flex flex-row">
             <div className="grow w-1/6 overflow-hidden mb-2.5">
