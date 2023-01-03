cd46d9b2dcebf1dca219b0d2afe512430176bda6
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-03 16:12:56 +0200

    Add missing file to previous commit

--- sky-core/src/components/notes/Note.js
+++ sky-core/src/components/notes/Note.js
@@ -17,15 +17,21 @@ export default class Note extends React.Component {
   }
 
   handleSelectButtonClick() {
-    this.props.selectButtonClick(this.props.id);
+    this.props.selectButtonClick(this.props.id, this.props.name, this.props.id);
   }
 
   render() {
+    if (this.props.selected) console.log("SELECTED");
+
+    let classAttrs = this.props.selected
+      ? "flex flex-col mr-1 ml-1 bg-slate-400 space-y-2 shadow-md"
+      : "flex flex-col mr-1 ml-1 bg-slate-100 space-y-2 shadow-md";
+
     return (
-      <div className="flex flex-col mr-1 ml-1 bg-slate-100 space-y-2 shadow-md">
+      <div className={classAttrs}>
         <button
           onClick={this.handleSelectButtonClick}
-          className="bg-slate-100 hover:bg-gray-50 active:bg-gray-100 m-1 p-1"
+          className="bg-slate-200 hover:bg-slate-300 active:bg-slate-400 m-1 p-1"
         >
           <div className="flex flex-row items-center">
             <span className="font-normal tracking-normal">
@@ -33,7 +39,7 @@ export default class Note extends React.Component {
             </span>
             <button
               onClick={this.handleRemoveButtonClick}
-              className="bg-white hover:bg-gray-50 active:bg-gray-100 border border-black px-3 py-1 ml-auto rounded-full"
+              className="bg-white hover:bg-gray-50 active:bg-gray-10 px-3 py-1 ml-auto rounded-full"
             >
               x
             </button>
