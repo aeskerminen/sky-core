ad9acd57e209743094b81fd5fb24407069a34c5c
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2023-01-05 18:02:39 +0200

    Add widgets

--- sky-core/src/components/widgets/Drawboard.js
+++ sky-core/src/components/widgets/Drawboard.js
@@ -1,20 +1,74 @@
 import React from "react";
-
+import CanvasDraw from "react-canvas-draw";
 
 export default class Drawboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      c_width: props.width,
+      c_height: props.height,
+      brushColor: "#3B82F6",
+      brushRadius: 5,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            c_width: props.width,
-            c_height: props.height
-        }
-    }
+  render() {
+    if (this.props.displayDrawboard) {
+      return (
+        <div className="p-2 bg-slate-100 w-full">
+          <CanvasDraw
+            ref={(canvasDraw) => (this.modify = canvasDraw)}
+            canvasWidth={this.state.c_width}
+            canvasHeight={this.state.c_height}
+            brushColor={this.state.brushColor}
+            brushRadius={this.state.brushRadius}
+            hideGrid={true}
+            enablePanAndZoom={true}
+          />
+          <div>
+            <button
+              className="pr-10"
+              onClick={() => {
+                this.modify.undo();
+              }}
+            >
+              UNDO
+            </button>
+            <button
+              onClick={() => {
+                this.modify.eraseAll();
+              }}
+            >
+              CLEAR
+            </button>
 
-    render() {
-        return(
-            <div className="p-2 bg-slate-100 w-full">
-            </div>
-        )
+            <input
+              className="block"
+              value={this.state.brushRadius}
+              type="range"
+              min="2"
+              max="50"
+              onChange={(e) => {
+                this.setState({
+                  brushRadius: e.target.value,
+                });
+              }}
+            />
+          </div>
+          <div>
+            {/* <label>Color picker</label> */}
+            <input
+              type="color"
+              value={this.state.brushColor}
+              onChange={(e) => {
+                this.setState({
+                  brushColor: e.target.value,
+                });
+              }}
+            />
+          </div>
+        </div>
+      );
     }
+  }
 }
---
+++ sky-core/src/components/widgets/Timer.js
@@ -0,0 +1,157 @@
+import React, { Component } from "react";
+
+export default class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
+
+  startTimer() {
+    this.myInterval = setInterval(() => {
+      const { hours, minutes, seconds } = this.state;
+
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1,
+        }));
+      }
+      if (seconds === 0) {
+        if (minutes === 0 && hours === 0) {
+          clearInterval(this.myInterval);
+        } else {
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59,
+          }));
+        }
+      }
+      if (minutes === 0 && hours != 0) {
+        this.setState(({ hours }) => ({
+          hours: hours - 1,
+          minutes: 59,
+          seconds: 59,
+        }));
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // destroyed
+    clearInterval(this.myInterval);
+  }
+
+  reset = () => {
+    this.setState({
+      isActive: false,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  };
+
+  turnOn = () => {
+    this.startTimer();
+    this.setState(() => ({
+      isActive: true,
+    }));
+  };
+
+  turnOff = () => {
+    clearInterval(this.myInterval);
+    this.setState(() => ({
+      isActive: false,
+    }));
+  };
+
+  incrUpH = () => {
+    this.setState(({ hours }) => ({
+      hours: hours + 1,
+    }));
+  };
+
+  incrUpM = () => {
+    this.state.minutes === 59
+      ? this.setState(() => ({ minutes: 0 }))
+      : this.setState(({ minutes }) => ({ minutes: minutes + 1 }));
+  };
+
+  incrUpS = () => {
+    this.state.seconds === 59
+      ? this.setState(() => ({ seconds: 0 }))
+      : this.setState(({ seconds }) => ({ seconds: seconds + 1 }));
+  };
+
+  incrDownH = () => {
+    this.state.hours === 0
+      ? this.setState(({ hours }) => ({ hours: hours }))
+      : this.setState(({ hours }) => ({ hours: hours - 1 }));
+  };
+
+  incrDownM = () => {
+    this.state.minutes === 0
+      ? this.setState(() => ({ minutes: 59 }))
+      : this.setState(({ minutes }) => ({ minutes: minutes - 1 }));
+  };
+
+  incrDownS = () => {
+    this.state.seconds === 0
+      ? this.setState(() => ({ seconds: 59 }))
+      : this.setState(({ seconds }) => ({ seconds: seconds - 1 }));
+  };
+
+  render() {
+    if (this.props.displayTimer) {
+      const { isActive, hours, minutes, seconds } = this.state;
+      return (
+        <div>
+          <div>
+            <button onClick={this.incrUpH}>
+              <small>⌃&nbsp;&nbsp;</small>
+            </button>
+            <button onClick={this.incrUpM}>
+              <small>⌃&nbsp;</small>
+            </button>
+            <button onClick={this.incrUpS}>
+              <small>&nbsp;&nbsp;⌃</small>
+            </button>
+          </div>
+
+          <div>
+            <div>
+              {minutes === 0 && seconds === 0 && hours === 0 ? (
+                <h1>00:00:00 ⏯⏹</h1>
+              ) : (
+                <h1>
+                  {hours}:{minutes < 10 ? `0${minutes}` : minutes}:
+                  {seconds < 10 ? `0${seconds}` : seconds}
+                  <button onClick={isActive ? this.turnOff : this.turnOn}>
+                    ⏯
+                  </button>
+                  <button onClick={this.reset}>⏹</button>
+                </h1>
+              )}
+            </div>
+            <div></div>
+          </div>
+
+          <div>
+            <button onClick={this.incrDownH}>
+              <small>⌄&nbsp;&nbsp;</small>
+            </button>
+            <button onClick={this.incrDownM}>
+              <small>⌄&nbsp;</small>
+            </button>
+            <button onClick={this.incrDownS}>
+              <small>&nbsp;&nbsp;⌄</small>
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
+}
