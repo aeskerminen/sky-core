37ccf322bf0b67fd9577d051c857a7d8c7fd7783
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-05 22:25:25 +0200

    Refactor Sidebar into functional component

--- sky-core/src/components/Sidebar.js
+++ sky-core/src/components/Sidebar.js
@@ -1,103 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./notes/Note";
 
 import { v4 as uuidv4 } from "uuid";
 
-export default class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: props.notes,
-      input_state: "",
-      selected: "",
-    };
+const Sidebar = (props) => {
+  let [notes, setNotes] = useState(props.notes);
 
-    console.log(this.state.notes);
+  let [input, setInput] = useState("");
+  let [selected, setSelected] = useState("");
 
-    this.addNote = this.addNote.bind(this);
-    this.removeNote = this.removeNote.bind(this);
-    this.handleSelectButtonClick = this.handleSelectButtonClick.bind(this);
+  function addNote(note) {
+    setNotes((notes = [...notes, note]));
   }
 
-  addNote(note) {
-    this.setState((state) => ({
-      notes: [...state.notes, note],
-      input_state: state.input_state,
-      selecetd: state.selected,
-    }));
-  }
-
-  removeNote(note) {
+  function removeNote(note) {
     console.log(note);
 
-    this.setState((state) => ({
-      notes: state.notes.filter((_note) => {
+    setNotes(
+      (notes = notes.filter((_note) => {
         return _note.id !== note.id;
-      }),
-      input_state: state.input_state,
-      selecetd: state.selected,
-    }));
+      }))
+    );
 
-    this.props.dbDeleteNotes(note.id);
+    props.dbDeleteNotes(note.id);
   }
 
-  handleInputChange(event) {
-    this.setState((state) => ({
-      notes: state.notes,
-      input_state: event.target.value,
-      selecetd: state.selected,
-    }));
+  function handleInputChange(event) {
+    setInput(event.target.value);
   }
 
-  handleSelectButtonClick(name, id, key) {
-    this.setState((state) => ({
-      notes: state.notes,
-      input_state: state.input_state,
-      selected: key,
-    }));
-
-    this.props.selectButtonClick(name, id);
+  function handleSelectButtonClick(name, id, key) {
+    setSelected(key);
+    props.selectButtonClick(name, id);
   }
 
-  render() {
-    return (
-      <div className="flex flex-col bg-slate-300 h-full p-2 shadow-md overflow-hidden">
-        <div className=" flex items-center p-1.5 bg-white">
-          <input
-            onChange={this.handleInputChange.bind(this)}
-            className="placeholder:text-slate-400 block w-full mr-2 bg-white border 
+  return (
+    <div className="flex flex-col bg-slate-300 h-full p-2 shadow-md overflow-hidden">
+      <div className=" flex items-center p-1.5 bg-white">
+        <input
+          onChange={handleInputChange}
+          className="placeholder:text-slate-400 block w-full mr-2 bg-white border 
                     border-black rounded-lg py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:ring-1 
                     sm:text-sm"
-            placeholder="Search for anything..."
-            type="text"
-            name="search"
-          />
+          placeholder="Search for anything..."
+          type="text"
+          name="search"
+        />
 
-          <button
-            onClick={() => {
-              this.addNote({
-                name: this.state.input_state,
-                id: uuidv4(),
-              });
-            }}
-            className="p-2 bg-white rounded-lg ml-auto hover:bg-slate-100"
-          >
-            ADD
-          </button>
-        </div>
-        <div className="grow flex flex-col p-2 gap-3 bg-white overflow-y-scroll">
-          {this.state.notes.map((note) => (
-            <Note
-              key={note.id}
-              selected={note.id === this.state.selected}
-              selectButtonClick={this.handleSelectButtonClick}
-              removeButtonClick={this.removeNote}
-              id={note.id}
-              name={note.name}
-            ></Note>
-          ))}
-        </div>
+        <button
+          onClick={() => {
+            addNote({
+              name: input,
+              id: uuidv4(),
+            });
+          }}
+          className="p-2 bg-white rounded-lg ml-auto hover:bg-slate-100"
+        >
+          ADD
+        </button>
       </div>
-    );
-  }
-}
+      <div className="grow flex flex-col p-2 gap-3 bg-white overflow-y-scroll">
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            selected={note.id === selected}
+            selectButtonClick={handleSelectButtonClick}
+            removeButtonClick={removeNote}
+            id={note.id}
+            name={note.name}
+          ></Note>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
