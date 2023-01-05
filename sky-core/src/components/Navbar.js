efabc4220463903e51278154c7053d8f27d1dca6
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2023-01-05 18:07:22 +0200

    Add timer, stopwatch

--- sky-core/src/components/Navbar.js
+++ sky-core/src/components/Navbar.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import LogoutButton from "./login/LogoutButton";
+import Timer from "./widgets/Timer";
+import Stopwatch from "./widgets/Stopwatch";
 
 const Navbar = () => {
   const { user } = useAuth0();
+  const [displayStopwatch, setDisplayStopwatch] = useState(true);
+  const [displayTimer, setDisplayTimer] = useState(true);
 
   return (
     <div className="flex items-center p-3 bg-blue-500 shadow-lg">
@@ -14,6 +18,8 @@ const Navbar = () => {
       <div className="flex items-center flex-shrink-1 text-white mr-6">
         <span className="font-semibold text-2xl tracking-normal">Sky Core</span>
       </div>
+      <Timer displayTimer={displayTimer} />
+      <Stopwatch displayStopwatch={displayStopwatch} />
       <div className="flex ml-auto">
         <button
           type="button"
