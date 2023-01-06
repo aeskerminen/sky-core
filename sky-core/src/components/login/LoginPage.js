60f95e0c6dfc1885ca7d36b5c5c50df7fde1760e
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2023-01-06 21:04:15 +0200

    Add logo

--- sky-core/src/components/login/LoginPage.js
+++ sky-core/src/components/login/LoginPage.js
@@ -15,13 +15,51 @@ const LoginPage = () => {
         <div className="p-2 h-32 bg-blue-500 shadow-md rounded-md flex">
           <div className="flex items-center flex-shrink-1 text-white mr-6">
             <svg
-              className="fill-current h-16 w-16 mr-4 ml-4"
-              width="54"
-              height="54"
-              viewBox="0 0 54 54"
+              className="mt-20"
+              id="logo"
+              width="419"
+              height="187"
+              viewBox="0 0 419 187"
+              fill="white"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+              <circle cx="65.5" cy="21.5" r="10" stroke="white" />
+              <circle cx="229.5" cy="10.5" r="10" stroke="white" />
+              <circle cx="187.5" cy="69.5" r="10" stroke="white" />
+              <circle cx="157.5" cy="37.5" r="10" stroke="white" />
+              <circle cx="105.5" cy="59.5" r="10" stroke="white" />
+              <line
+                x1="72.6957"
+                y1="28.163"
+                x2="98.6957"
+                y2="52.163"
+                stroke="white"
+                stroke-width="5"
+              />
+              <line
+                x1="114.991"
+                y1="56.7127"
+                x2="148.991"
+                y2="41.7127"
+                stroke="white"
+                stroke-width="5"
+              />
+              <line
+                x1="164.869"
+                y1="44.3391"
+                x2="180.869"
+                y2="62.3391"
+                stroke="white"
+                stroke-width="5"
+              />
+              <line
+                x1="191.972"
+                y1="60.538"
+                x2="222.972"
+                y2="17.538"
+                stroke="white"
+                stroke-width="5"
+              />
             </svg>
             <span className="font-semibold text-7xl tracking-normal">
               Sky Core
