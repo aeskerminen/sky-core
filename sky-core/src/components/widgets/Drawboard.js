fb22e10a4e0c4c134d90cb7c8d79f6f37b1e9226
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-08 16:04:05 +0200

    Fix absolute positioning bug

--- sky-core/src/components/widgets/Drawboard.js
+++ sky-core/src/components/widgets/Drawboard.js
@@ -13,62 +13,60 @@ export default class Drawboard extends React.Component {
   }
 
   render() {
-    if (this.props.displayDrawboard) {
-      return (
-        <div className="p-2 bg-slate-100 w-full">
-          <CanvasDraw
-            ref={(canvasDraw) => (this.modify = canvasDraw)}
-            canvasWidth={this.state.c_width}
-            canvasHeight={this.state.c_height}
-            brushColor={this.state.brushColor}
-            brushRadius={this.state.brushRadius}
-            hideGrid={true}
-            enablePanAndZoom={true}
-          />
-          <div>
-            <button
-              className="pr-10"
-              onClick={() => {
-                this.modify.undo();
-              }}
-            >
-              UNDO
-            </button>
-            <button
-              onClick={() => {
-                this.modify.eraseAll();
-              }}
-            >
-              CLEAR
-            </button>
+    return (
+      <div className="p-2 bg-slate-100 relative">
+        <CanvasDraw
+          ref={(canvasDraw) => (this.modify = canvasDraw)}
+          canvasWidth={this.state.c_width}
+          canvasHeight={this.state.c_height}
+          brushColor={this.state.brushColor}
+          brushRadius={this.state.brushRadius}
+          hideGrid={true}
+          enablePanAndZoom={false}
+        />
+        <div>
+          <button
+            className="pr-10"
+            onClick={() => {
+              this.modify.undo();
+            }}
+          >
+            UNDO
+          </button>
+          <button
+            onClick={() => {
+              this.modify.eraseAll();
+            }}
+          >
+            CLEAR
+          </button>
 
-            <input
-              className="block"
-              value={this.state.brushRadius}
-              type="range"
-              min="2"
-              max="50"
-              onChange={(e) => {
-                this.setState({
-                  brushRadius: e.target.value,
-                });
-              }}
-            />
-          </div>
-          <div>
-            {/* <label>Color picker</label> */}
-            <input
-              type="color"
-              value={this.state.brushColor}
-              onChange={(e) => {
-                this.setState({
-                  brushColor: e.target.value,
-                });
-              }}
-            />
-          </div>
+          <input
+            className="block"
+            value={this.state.brushRadius}
+            type="range"
+            min="2"
+            max="50"
+            onChange={(e) => {
+              this.setState({
+                brushRadius: e.target.value,
+              });
+            }}
+          />
         </div>
-      );
-    }
+        <div>
+          {/* <label>Color picker</label> */}
+          <input
+            type="color"
+            value={this.state.brushColor}
+            onChange={(e) => {
+              this.setState({
+                brushColor: e.target.value,
+              });
+            }}
+          />
+        </div>
+      </div>
+    );
   }
 }
