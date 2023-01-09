d4cedad70393004435f08d5a21f34f4eb15279cf
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-09 17:20:06 +0200

    Style heavy-widget buttons

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -146,37 +146,79 @@ const App = () => {
 
                   {displayPDF && <PDFViewer />}
 
-                  <div className="">
+                  <div className="flex flex-row justify-center gap-x-2 m-2">
                     <button
                       id="pdfbtn"
                       title="PDF Viewer"
-                      className=""
+                      className="p-2 bg-white rounded-md shadow-md"
                       onClick={() => {
                         setDisplayPDF(!displayPDF);
-                        const elem = document.getElementById("pdfbtn");
-
-                        elem.innerHTML === "+"
-                          ? (elem.innerHTML = "-")
-                          : (elem.innerHTML = "+");
                       }}
                     >
-                      +
+                      {!displayPDF && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-plus-lg"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                          />
+                        </svg>
+                      )}
+                      {displayPDF && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-x-lg"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                        </svg>
+                      )}
                     </button>
 
                     <button
                       id="drawboardbtn"
                       title="Drawboard"
-                      className="block"
+                      className="p-2 bg-white rounded-md shadow-md"
                       onClick={() => {
                         setDisplayDrawboard(!displayDrawboard);
-                        const elem = document.getElementById("drawboardbtn");
-
-                        elem.innerHTML === "+"
-                          ? (elem.innerHTML = "-")
-                          : (elem.innerHTML = "+");
                       }}
                     >
-                      +
+                      {!displayDrawboard && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-plus-lg"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                          />
+                        </svg>
+                      )}
+                      {displayDrawboard && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-x-lg"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                        </svg>
+                      )}
                     </button>
                   </div>
                 </div>
