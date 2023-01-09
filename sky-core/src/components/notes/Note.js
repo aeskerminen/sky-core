ab1aae6fb95c95174da7b182d168f432db3f556a
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-09 16:27:26 +0200

    Style note

--- sky-core/src/components/notes/Note.js
+++ sky-core/src/components/notes/Note.js
@@ -10,22 +10,28 @@ const Note = (props) => {
   }
 
   let classAttrs = props.selected
-    ? "flex flex-col mr-1 ml-1 bg-slate-400 space-y-2 shadow-md"
-    : "flex flex-col mr-1 ml-1 bg-slate-100 space-y-2 shadow-md";
+    ? "flex flex-col mr-1 ml-1 bg-slate-400 hover:bg-slate-300 space-y-2 shadow-md rounded-lg"
+    : "flex flex-col mr-1 ml-1 bg-slate-100 hover:bg-slate-300 space-y-2 shadow-md rounded-lg";
 
   return (
     <div className={classAttrs}>
-      <button
-        onClick={handleSelectButtonClick}
-        className="bg-slate-200 hover:bg-slate-300 active:bg-slate-400 m-1 p-1"
-      >
+      <button onClick={handleSelectButtonClick} className="m-1 p-1">
         <div className="flex flex-row items-center">
           <span className="font-normal tracking-normal">{props.name}</span>
           <button
             onClick={handleRemoveButtonClick}
-            className="bg-white hover:bg-gray-50 active:bg-gray-10 px-3 py-1 ml-auto rounded-full"
+            className="bg-white hover:bg-red-400 active:bg-gray-10 p-2 ml-auto rounded-full"
           >
-            x
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-x-lg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
           </button>
         </div>
       </button>
