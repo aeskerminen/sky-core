4a8b9f9c1c07e17783f020dd4bf08ec719f82412
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-09 17:34:24 +0200

    Styling changes

--- sky-core/src/components/Navbar.js
+++ sky-core/src/components/Navbar.js
@@ -15,46 +15,89 @@ const Navbar = () => {
       className="flex items-center p-3 bg-blue-500 shadow-lg"
       style={{ caretColor: "transparent" }}
     >
-      <div className="flex mr-auto">
-        <LogoutButton></LogoutButton>
+      <LogoutButton></LogoutButton>
+      <div className="flex flex-row justify-center ml-auto">
+        <div className="flex gap-x-2">
+          <Timer displayTimer={displayTimer} />
+          <div className="flex flex-row gap-x-2">
+            <button
+              id="timerbtn"
+              title="Timer"
+              className="bg-white rounded-md shadow-md p-1"
+              onClick={() => {
+                setDisplayTimer(!displayTimer);
+              }}
+            >
+              {!displayTimer && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-plus-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                  />
+                </svg>
+              )}
+              {displayTimer && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-x-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                </svg>
+              )}
+            </button>
+            <button
+              id="stopwatchbtn"
+              title="Stopwatch"
+              className="bg-white rounded-md shadow-md p-1"
+              onClick={() => {
+                setDisplayStopwatch(!displayStopwatch);
+                const elem = document.getElementById("stopwatchbtn");
+              }}
+            >
+              {!displayStopwatch && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-plus-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                  />
+                </svg>
+              )}
+              {displayStopwatch && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-x-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                </svg>
+              )}
+            </button>
+          </div>
+          <Stopwatch displayStopwatch={displayStopwatch} />
+        </div>
       </div>
-      <div className="flex items-center flex-shrink-1 text-white mr-6">
-        <span className="font-semibold text-2xl tracking-normal">
-          Sky Notes
-        </span>
-      </div>
-      <Timer displayTimer={displayTimer} />
-      <Stopwatch displayStopwatch={displayStopwatch} />
-      <div className="">
-        <button
-          id="timerbtn"
-          title="Timer"
-          className="block pr-5"
-          onClick={() => {
-            setDisplayTimer(!displayTimer);
-            const elem = document.getElementById("timerbtn");
-            elem.innerHTML === "+"
-              ? (elem.innerHTML = "-")
-              : (elem.innerHTML = "+");
-          }}
-        >
-          +
-        </button>
-        <button
-          id="stopwatchbtn"
-          title="Stopwatch"
-          className=""
-          onClick={() => {
-            setDisplayStopwatch(!displayStopwatch);
-            const elem = document.getElementById("stopwatchbtn");
-            elem.innerHTML === "+"
-              ? (elem.innerHTML = "-")
-              : (elem.innerHTML = "+");
-          }}
-        >
-          +
-        </button>
-      </div>
+
       <div className="flex ml-auto">
         <button
           type="button"
