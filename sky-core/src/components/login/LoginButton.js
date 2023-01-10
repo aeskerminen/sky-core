0bb9f78ad8fd262eccda2bf276c255f3cefac834
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-10 23:12:49 +0200

    Add styling

--- sky-core/src/components/login/LoginButton.js
+++ sky-core/src/components/login/LoginButton.js
@@ -5,7 +5,10 @@ const LoginButton = () => {
 
   return (
     !isAuthenticated && (
-      <div className="flex p-1 align-middle justify-center w-60 bg-blue-500 rounded-md shadow-md">
+      <div
+        style={{ caretColor: "transparent" }}
+        className="flex p-1 align-middle justify-center w-60 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded-md shadow-md"
+      >
         <button
           className="align-middle p-2"
           onClick={() => loginWithRedirect()}
