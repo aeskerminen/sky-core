ca36feb1a13c5ba6eaec3aac0f2530bebfba5dc0
Joonatan <joonatan.merenluoto@gmail.com>
2023-01-15 23:00:24 +0200

    Add color picker

--- sky-core/src/components/widgets/Drawboard.js
+++ sky-core/src/components/widgets/Drawboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CanvasDraw from "react-canvas-draw";
+import { CompactPicker } from "react-color";
 
 export default class Drawboard extends React.Component {
   constructor(props) {
@@ -11,6 +12,10 @@ export default class Drawboard extends React.Component {
       brushRadius: 5,
     };
   }
+  handleChangeComplete = (color) => {
+    this.setState({ brushColor: color.hex });
+  };
+
   render() {
     return (
       <div className="p-2 mt-2 bg-slate-100 relative">
@@ -44,13 +49,12 @@ export default class Drawboard extends React.Component {
             className="p-2 bg-white shadow-md rounded-md"
             onClick={() => {
               /*
-              localStorage.setItem(
-                "savedDrawing",
-                this.modify.getDataURL()
-              ); */
+localStorage.setItem(
+"savedDrawing",
+this.modify.getDataURL()
+); */
               var a = document.createElement("a");
-              a.href =
-              this.modify.getDataURL();
+              a.href = this.modify.getDataURL();
               a.download = "Drawing.png";
               a.click();
             }}
@@ -60,13 +64,12 @@ export default class Drawboard extends React.Component {
 
           <div className="p-2 bg-white shadow-md rounded-md">
             <input
-              style={{accentColor: this.state.brushColor}}
+              style={{ accentColor: this.state.brushColor }}
               className="align-middle"
               value={this.state.brushRadius}
               type="range"
               min="2"
               max="50"
-              
               onChange={(e) => {
                 this.setState({
                   brushRadius: e.target.value,
@@ -76,15 +79,9 @@ export default class Drawboard extends React.Component {
             />
           </div>
           <div className="p-2 bg-white shadow-md rounded-md">
-            <input
-              className="align-middle"
-              type="color"
-              value={this.state.brushColor}
-              onChange={(e) => {
-                this.setState({
-                  brushColor: e.target.value,
-                });
-              }}
+            <CompactPicker
+              color={this.state.brushColor}
+              onChangeComplete={this.handleChangeComplete}
             />
           </div>
         </div>
