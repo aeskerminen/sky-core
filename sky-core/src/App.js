c5502173e1efc825e133c9dc7467988138260dbf
Artturi Kerminen <kerminenartturi@gmail.com>
2023-01-15 20:57:43 +0200

    Add svg wrappers

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -24,6 +24,7 @@ import "react-quill/dist/quill.snow.css";
 // LaTeX
 import katex from "katex";
 import "katex/dist/katex.min.css";
+import { PLUS_ICON, X_ICON } from "./helpers/icons";
 
 window.katex = katex;
 
@@ -194,33 +195,8 @@ const App = () => {
                         setDisplayPDF(!displayPDF);
                       }}
                     >
-                      {!displayPDF && (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-plus-lg"
-                          viewBox="0 0 16 16"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-                          />
-                        </svg>
-                      )}
-                      {displayPDF && (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-x-lg"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-                        </svg>
-                      )}
+                      {!displayPDF && <PLUS_ICON></PLUS_ICON>}
+                      {displayPDF && <X_ICON></X_ICON>}
                     </button>
 
                     <button
@@ -231,33 +207,8 @@ const App = () => {
                         setDisplayDrawboard(!displayDrawboard);
                       }}
                     >
-                      {!displayDrawboard && (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-plus-lg"
-                          viewBox="0 0 16 16"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-                          />
-                        </svg>
-                      )}
-                      {displayDrawboard && (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          class="bi bi-x-lg"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-                        </svg>
-                      )}
+                      {!displayDrawboard && <PLUS_ICON></PLUS_ICON>}
+                      {displayDrawboard && <X_ICON></X_ICON>}
                     </button>
                   </div>
                 </div>
---
+++ sky-core/src/helpers/icons.js
@@ -0,0 +1,32 @@
+export const PLUS_ICON = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-plus-lg"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+      />
+    </svg>
+  );
+};
+
+export const X_ICON = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-x-lg"
+      viewBox="0 0 16 16"
+    >
+      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+    </svg>
+  );
+};
