b345fe6f1416637bdcff825386704f828f115ffd
Joonatan <joonatan.merenluoto@gmail.com>
2023-01-16 01:36:11 +0200

    Change layout

--- sky-core/src/components/login/LogoutButton.js
+++ sky-core/src/components/login/LogoutButton.js
@@ -2,15 +2,20 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { SIGNOUT_ICON } from "../../helpers/icons";
 
 const LogoutButton = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const { user, logout, isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && (
       <div
-        className="bg-blue-700 hover:bg-blue-800 cursor-pointer active:bg-blue-900 shadow-md rounded-md text-white mr-2 p-2"
+        title="Log Out"
+        className="flex flew-row bg-blue-700 hover:bg-blue-800 cursor-pointer active:bg-blue-900 rounded-md text-white"
         onClick={() => logout()}
       >
-        <SIGNOUT_ICON></SIGNOUT_ICON>
+        <div className="pr-2 pt-0.5">
+          <SIGNOUT_ICON></SIGNOUT_ICON>
+        </div>
+
+        {user?.name}
       </div>
     )
   );
