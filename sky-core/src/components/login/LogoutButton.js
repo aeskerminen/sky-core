87a6e8adb501f62ea428631097683674610d525a
Artturi Kerminen <kerminenartturi@gmail.com>
2023-01-15 21:57:02 +0200

    Add sign out icon

--- sky-core/src/components/login/LogoutButton.js
+++ sky-core/src/components/login/LogoutButton.js
@@ -1,16 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { SIGNOUT_ICON } from "../../helpers/icons";
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && (
-      <div className="bg-blue-700 hover:bg-blue-800 active:bg-blue-900 shadow-md rounded-md">
-        <button className="" onClick={() => logout()}>
-          <h1 className=" text-white p-1.5" style={{ textAlign: "center" }}>
-            Sign out
-          </h1>
-        </button>
+      <div
+        className="bg-blue-700 hover:bg-blue-800 cursor-pointer active:bg-blue-900 shadow-md rounded-md text-white mr-2 p-2"
+        onClick={() => logout()}
+      >
+        <SIGNOUT_ICON></SIGNOUT_ICON>
       </div>
     )
   );
