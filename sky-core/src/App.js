6a99ee3f8a19a06fc8696b6d81612c14b09f5089
Artturi Kerminen <kerminenartturi@gmail.com>
2023-01-15 19:22:53 +0200

    Fix spacing on full width editor

--- sky-core/src/App.js
+++ sky-core/src/App.js
@@ -139,7 +139,7 @@ const App = () => {
             </div>
             <div
               className={`${
-                showSidebar ? "w-5/6" : "w-full"
+                showSidebar ? "w-5/6" : "w-full ml-2"
               } bg-white overflow-y-scroll p-2 m-2 mb-3 ml-1 rounded-md bound`}
               style={{ caretColor: "transparent" }}
             >
