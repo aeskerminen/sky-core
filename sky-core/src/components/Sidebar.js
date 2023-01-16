a59fd9bc590dc30bce1c626cc60fee64f830ca43
Artturi Kerminen <a.kerminen11@gmail.com>
2023-01-16 18:16:04 +0200

    Style changes

--- sky-core/src/components/Sidebar.js
+++ sky-core/src/components/Sidebar.js
@@ -35,11 +35,11 @@ const Sidebar = (props) => {
 
   return (
     <div className="flex flex-col h-full overflow-hidden p-2 bg-white">
-      <div className=" flex items-center p-1.5">
+      <div className=" flex items-center p-1.5 bg-blue-500 mb-2 rounded-md shadow">
         <input
           onChange={handleInputChange}
-          className="placeholder:text-slate-400 block w-full mr-2 bg-white border-2
-                    border-black rounded-lg py-2 pl-3 pr-3 shadow focus:outline-none focus:ring-1 
+          className="placeholder:text-slate-400 block w-full mr-2 bg-white
+                    rounded-lg py-2 pl-3 pr-3 focus:outline-none focus:ring-1 
                     sm:text-sm"
           placeholder="Name your note..."
           type="text"
